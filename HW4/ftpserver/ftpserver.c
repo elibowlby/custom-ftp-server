c86c3d4c6e66ced488b0f5034e558998e266daa5
Eli Bowlby <elijahbowlby@hotmail.com>
2022-11-04 11:24:17 -0500

    dd

---
+++ HW4/ftpserver/ftpserver.c
@@ -0,0 +1,60 @@
+// Elijah Bowlby
+// CSCI 3550
+// HW 4
+// ftpserver.c
+// 10/21/2022
+
+
+// UDP echo server program
+#include "headerFile.h"
+
+int main(void)
+{
+    // Declare and define variables
+    // Socket descriptor (reference)
+    int s;
+    // Number of bytes to send or receive
+    int len;
+    // Data buffer
+    char buffer[256];
+    // Server socket address
+    struct sockaddr_in servAddr;
+    // Client socket address
+    struct sockaddr_in clntAddr;
+    // Length of client socket address
+    int clntAddrLen;
+
+    // Build local (server) socket address
+    // Set socket structure all to 0
+    memset(&servAddr, 0, sizeof(servAddr));
+    // Family field
+    servAddr.sin_family = AF_INET;
+    // Default port number
+    servAddr.sin_port = htons(52678);
+    // Default IP address
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    // Create listen socket
+    s = socket(PF_INET, SOCK_DGRAM, 0);
+    if (s < 0)
+    {
+        perror("Error: Socket failed!\n");
+        exit(1);
+    }
+
+    // Bind listen socket to local address
+    if (bind(s, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
+    {
+        perror("Error: Bind failed!\n");
+        exit(1);
+    }
+
+    // Run forever
+    for(; ;)  
+    {
+        // Receive string
+        len = recvfrom(s, buffer, sizeof(buffer), 0, (struct sockaddr *)&clntAddr, &clntAddrLen);
+        // Send string
+        sendto(s, buffer, len, 0, (struct sockaddr *)&clntAddr, sizeof(clntAddr));
+    } // End of for loop
+} // End of echo server program
---
+++ HW4/ftpserver/headerFile.h
@@ -0,0 +1,45 @@
+// Elijah Bowlby
+// CSCI 3550
+// HW 4
+// headerFile.h
+// 10/21/2022
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <errno.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+
+
+// Structs are already defined in import libraries
+
+// struct sockaddr
+// {
+//     unsigned short
+//         // Address family - (2 bytes) holds the family and
+//         sa_family;
+//     char
+//         // Holds port (2 bytes), IP address (4 bytes), and 8 bytes of unused chars.
+//         sa_data[14];
+// };
+
+// struct sockaddr_in
+// {
+//     short sin_family;        // Address family
+//     unsigned short sin_port; // Port number
+//     struct in_addr sin_addr; // Internet (IP) address
+//     unsigned char sin_zero[8];
+// };
+
+// struct in_addr
+// {
+//     unsigned long s_addr; // The 32 bit IP address
+// };
